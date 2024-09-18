8ddcf013645bae94b6126c6ffc45fc32e119c87e
Hekalis <dark_angel_arch_angel@hotmail.com>
2024-09-18 15:51:01 +0100

    Language Learner Game

---
+++ Humble Intro to Coding/The Complete Kivy App Development Course/main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.properties import StringProperty, NumericProperty, ListProperty
+
+class GameScreen(Widget):
+    pass
+
+class LanguageLearnerApp(App):
+    def build(self):
+        game_screen = GameScreen()
+        return game_screen
+    
+if __name__ == "__main__":
+    LanguageLearnerApp().run()
